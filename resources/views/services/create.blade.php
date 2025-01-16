824c6e2203a2ab1603af8dcbf30d135013d80414
HP <HP@DESKTOP-QOEL64A.local>
2025-01-16 14:02:48 +0000

    show errors

--- resources/views/services/create.blade.php
+++ resources/views/services/create.blade.php
@@ -15,21 +15,30 @@
                                 <div class="col-md-12 mb-3">
                                     <div class="form-group">
                                         <label for="libelle">Libellé</label>
-                                        <input type="text" class="form-control" id="libelle" name="libelle" value="{{ old('libelle') }}" required>
+                                        <input type="text" class="form-control @error('libelle') is-invalid @enderror" id="libelle" name="libelle" value="{{ old('libelle') }}" required>
+                                        @error('libelle')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
                                 </div>
 
                                 <div class="col-md-12 mb-3">
                                     <div class="form-group">
                                         <label for="groupe">Groupe</label>
-                                        <input type="text" class="form-control" id="groupe" name="groupe" value="{{ old('groupe') }}" required>
+                                        <input type="text" class="form-control @error('groupe') is-invalid @enderror" id="groupe" name="groupe" value="{{ old('groupe') }}" required>
+                                        @error('groupe')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
                                 </div>
 
                                 <div class="col-md-12 mb-3">
                                     <div class="form-group">
                                         <label for="description">Description</label>
-                                        <textarea class="form-control" id="description" name="description" rows="3">{{ old('description') }}</textarea>
+                                        <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" rows="3">{{ old('description') }}</textarea>
+                                        @error('description')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
                                 </div>
 
@@ -37,11 +46,17 @@
                                     <h5>Informations réseau internes</h5>
                                     <div class="form-group mb-3">
                                         <label for="ip_interne">IP interne</label>
-                                        <input type="text" class="form-control" id="ip_interne" name="ip_interne" value="{{ old('ip_interne') }}">
+                                        <input type="text" class="form-control @error('ip_interne') is-invalid @enderror" id="ip_interne" name="ip_interne" value="{{ old('ip_interne') }}">
+                                        @error('ip_interne')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
                                     <div class="form-group mb-3">
                                         <label for="port_interne">Port interne</label>
-                                        <input type="text" class="form-control" id="port_interne" name="port_interne" value="{{ old('port_interne') }}">
+                                        <input type="text" class="form-control @error('port_interne') is-invalid @enderror" id="port_interne" name="port_interne" value="{{ old('port_interne') }}">
+                                        @error('port_interne')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
                                 </div>
 
@@ -49,15 +64,24 @@
                                     <h5>Informations réseau externes</h5>
                                     <div class="form-group mb-3">
                                         <label for="ip_publique">IP publique</label>
-                                        <input type="text" class="form-control" id="ip_publique" name="ip_publique" value="{{ old('ip_publique') }}">
+                                        <input type="text" class="form-control @error('ip_publique') is-invalid @enderror" id="ip_publique" name="ip_publique" value="{{ old('ip_publique') }}">
+                                        @error('ip_publique')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
                                     <div class="form-group mb-3">
                                         <label for="port_externe">Port externe</label>
-                                        <input type="text" class="form-control" id="port_externe" name="port_externe" value="{{ old('port_externe') }}">
+                                        <input type="text" class="form-control @error('port_externe') is-invalid @enderror" id="port_externe" name="port_externe" value="{{ old('port_externe') }}">
+                                        @error('port_externe')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
                                     <div class="form-group mb-3">
                                         <label for="adresse_dns">Adresse DNS</label>
-                                        <input type="text" class="form-control" id="adresse_dns" name="adresse_dns" value="{{ old('adresse_dns') }}">
+                                        <input type="text" class="form-control @error('adresse_dns') is-invalid @enderror" id="adresse_dns" name="adresse_dns" value="{{ old('adresse_dns') }}">
+                                        @error('adresse_dns')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
                                 </div>
                             </div>
@@ -67,12 +91,18 @@
                                     <h5>Paramètres additionnels</h5>
                                     <div class="d-flex gap-3">
                                         <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" id="is_api" name="is_api" {{ old('is_api') ? 'checked' : '' }}>
+                                            <input class="form-check-input @error('is_api') is-invalid @enderror" type="checkbox" id="is_api" name="is_api" {{ old('is_api') ? 'checked' : '' }}>
                                             <label class="form-check-label" for="is_api">Service API</label>
+                                            @error('is_api')
+                                                <div class="invalid-feedback">{{ $message }}</div>
+                                            @enderror
                                         </div>
                                         <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" id="admin_received" name="admin_received" {{ old('admin_received') ? 'checked' : '' }}>
+                                            <input class="form-check-input @error('admin_received') is-invalid @enderror" type="checkbox" id="admin_received" name="admin_received" {{ old('admin_received') ? 'checked' : '' }}>
                                             <label class="form-check-label" for="admin_received">Admin reçu</label>
+                                            @error('admin_received')
+                                                <div class="invalid-feedback">{{ $message }}</div>
+                                            @enderror
                                         </div>
                                     </div>
                                 </div>
